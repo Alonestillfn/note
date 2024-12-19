f3e1d07f2630d22e04db3f2d88966c9c3d1f7cbb
Huynh Van Tai <vawnitaf1307@gmail.com>
2024-12-19 21:02:48 +0700

    Thêm các file

---
+++ src/Wrap/Head/Head.js
@@ -0,0 +1,20 @@
+import '/Head.css'
+
+function Head()
+{
+    return (
+        <div className='header'>
+            <div className='primaryName'>
+                My Diary
+            </div>
+            <div className='navbar'>
+                <ul>
+                    <li>Diaries</li>
+                    <li>Setting</li>
+                    <li>Theme</li>
+                    <li>Diaries</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
---
+++ src/Wrap/wrap.js
@@ -0,0 +1,10 @@
+import './wrap.css'
+
+function Wrap({ children })
+{
+    return (
+        <div className="wrap">{children}</div>
+    )
+}
+
+export default Wrap
